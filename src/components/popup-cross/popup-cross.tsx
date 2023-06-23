89e21ddc6f1616bc916c7bed30d813761fd06208
dariarus <rusanova_dv@mail.ru>
2023-06-23 03:44:24 +0300

    creating and styling cross-icon for popup

---
+++ src/components/popup-cross/popup-cross.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import popupCrossStyles from './popup-cross.module.css';
+
+export const PopupCross = () => {
+  return (
+    <button className={popupCrossStyles.icon}>
+      {/*<img className={popupCrossStyles.icon} src={crossIcon} alt="Иконка крестик"/>*/}
+    </button>
+
+  )
+}
--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import popupStyles from './popup.module.css';
 
 import {Overlay} from '../overlay/overlay';
+import {PopupCross} from '../popup-cross/popup-cross';
 
 export const Popup = () => {
   const popupRoot = document.getElementById('popup');
@@ -14,16 +15,19 @@ export const Popup = () => {
         <>
           <Overlay/>
           <div className={popupStyles.popup}>
+            <PopupCross/>
             <h2 className={popupStyles.popupHeading}>Игра "Пазлы"</h2>
-            <p className={popupStyles.popupText}>Цель игры - собрать картинку из ее перемешанных фрагментов.
+            <div className={popupStyles.popupTextWrap}>
+              <p className={popupStyles.popupText}>Цель игры - собрать картинку из ее перемешанных фрагментов.
               Чтобы это сделать, перетащите фрагменты из левого блока на их места в блоке справа.</p>
-            <p className={popupStyles.popupText}>Если вы неправильно установили фрагмент, его можно перетащить снова:<br/>
-            а) внутри блока с готовой картинкой;<br/>
-            б) обратно в блок с неиспользованными фрагментами.</p>
-            <p className={popupStyles.popupText}>С помощью кнопки "Сбросить" можно начать с начала:
-              уже перенесенные в блок с картинкой фрагменты снова окажутся в исходном блоке.</p>
-            <p className={popupStyles.popupText}>Если не хотите собирать предложенную картинку, ее можно поменять,
-              нажав кнопку "Сменить картинку"</p>
+              <p className={popupStyles.popupText}>Если вы неправильно установили фрагмент, его можно перетащить снова:<br/>
+                а) внутри блока с готовой картинкой;<br/>
+                б) обратно в блок с неиспользованными фрагментами.</p>
+              <p className={popupStyles.popupText}>С помощью кнопки "Сбросить" можно начать с начала:
+                уже перенесенные в блок с картинкой фрагменты снова окажутся в исходном блоке.</p>
+              <p className={popupStyles.popupText}>Если не хотите собирать предложенную картинку, ее можно поменять,
+                нажав кнопку "Сменить картинку"</p>
+            </div>
           </div>
         </>
       ), popupRoot
